ec3b16e632fcbba4a5565b506b9941ca2f6fdd41
Dan Suh <kehops@kaist.ac.kr>
2017-07-08 20:14:49 -0700

    Change production port.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -22,7 +22,7 @@ module.exports = {
     historyApiFallback: true,
     contentBase: '/public',
     proxy: {
-      '*': 'http://localhost:3000',
+      '*': 'http://localhost:4001',
     },
     stats: {
       assets: false,
