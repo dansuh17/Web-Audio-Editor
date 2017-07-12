d6157a1392c9f84d21de2c8ec46797e28ee84317
Dan Suh <kehops@kaist.ac.kr>
2017-07-11 18:56:16 -0700

    Upload and visualize

--- src/index.js
+++ src/index.js
@@ -1,8 +1,57 @@
 import '../index.html';  // required for hot-loading for changes in index.html
+import wavesUI from 'waves-ui';
+
+
+const fileinput = document.getElementById('file-input');
+fileinput.addEventListener('change', readSingleFile, false);
+
+function readSingleFile(e) {
+  "use strict";
+  const file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  const reader = new FileReader();
+
+  reader.onload = e => {
+    const contents = e.target.result;
+    drawWave(contents);
+  };
+
+  reader.readAsArrayBuffer(file);
+}
+
+
+function drawWave(fileArrayBuffer) {
+  "use strict";
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  const audioContext = new AudioContext();
+  audioContext.decodeAudioData(fileArrayBuffer, buffer => {
+    var $track = document.querySelector('#track-1');
+    var width = $track.getBoundingClientRect().width;
+    var height = 200;
+    var duration = buffer.duration;
+    // define the numbr of pixels per seconds the timeline should display
+    var pixelsPerSecond = width / duration;
+    // create a timeline
+    var timeline = new wavesUI.core.Timeline(pixelsPerSecond, width);
+    // create a new track into the `track-1` element and give it a id ('main')
+    timeline.createTrack($track, height, 'main');
+
+    // create the layer
+    var waveformLayer = new wavesUI.helpers.WaveformLayer(buffer, {
+      height: height
+    });
+
+    // insert the layer inside the 'main' track
+    timeline.addLayer(waveformLayer, 'main');
+  });
+}
+
 
 function component() {
   var element = document.createElement('div');
-  element.innerHTML = 'Shit';
+  element.innerHTML = 'Hello World';
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,20 +11,31 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',  // [name] is the entry key
     path: DIST_PATH,  // output path
-    publicPath: '/public',  // location of static files that would be requested
+    publicPath: '/public/',  // location of static files that would be requested
   },
   devtool: 'cheap-eval-source-map',
   devServer: {
     port: 8080,
     hot: true,  // tell the dev server to use HMR
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
     publicPath: '/dist',  // contents from webpack served from HERE
+  },
+  resolve: {
+    extensions: [".js"],
   },
   module: {
     loaders: [
       {
         test: /\.html$/,
         loader: 'html-loader',
-      }
+      },
+      {
+        test: /\.(jpg|mp3)$/,
+        loader: 'file-loader',
+      },
     ],
   },
   plugins: [
