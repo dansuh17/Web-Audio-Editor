2ad5b816a57a491e3604925f97844158be4d3936
Dan Suh <kehops@kaist.ac.kr>
2017-07-12 13:56:07 -0700

    Wrapped the tacks into classes. Function execution context binding.

--- src/index.js
+++ src/index.js
@@ -1,52 +1,54 @@
 import '../index.html';  // required for hot-loading for changes in index.html
 import wavesUI from 'waves-ui';
 
-
-const fileinput = document.getElementById('file-input');
-fileinput.addEventListener('change', readSingleFile, false);
-
-
-function readSingleFile(e) {
-  const file = e.target.files[0];
-  if (!file) {
-    return;
+class Track {
+  constructor() {
+    this.fileinput = document.getElementById('file-input');
+    this.readSingleFile = this.readSingleFile.bind(this);
+    this.fileinput.addEventListener('change', this.readSingleFile, false);
   }
-  const reader = new FileReader();
 
-  // create audio context - later will desireably become global singleton
-  const AudioContext = window.AudioContext || window.webkitAudioContext;
-  const audioCtx = new AudioContext();
+  readSingleFile(e) {
+    const file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
 
-  reader.onload = e => {
-    const contents = e.target.result;
-    drawWave(contents, audioCtx);
-  };
+    // create audio context - later will desireably become global singleton
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const audioCtx = new AudioContext();
 
-  reader.readAsArrayBuffer(file);
-}
+    reader.onload = e => {
+      const contents = e.target.result;
+      this.drawWave(contents, audioCtx);
+    };
 
+    reader.readAsArrayBuffer(file);
+  }
 
-function drawWave(fileArrayBuffer, audioCtx) {
-  audioCtx.decodeAudioData(fileArrayBuffer, buffer => {
-    var $track = document.querySelector('#track-1');
-    var width = $track.getBoundingClientRect().width;
-    var height = 200;
-    var duration = buffer.duration;
-    // define the numbr of pixels per seconds the timeline should display
-    var pixelsPerSecond = width / duration;
-    // create a timeline
-    var timeline = new wavesUI.core.Timeline(pixelsPerSecond, width);
-    // create a new track into the `track-1` element and give it a id ('main')
-    timeline.createTrack($track, height, 'main');
+  drawWave(fileArrayBuffer, audioCtx) {
+    audioCtx.decodeAudioData(fileArrayBuffer, buffer => {
+      var $track = document.querySelector('#track-1');
+      var width = $track.getBoundingClientRect().width;
+      var height = 200;
+      var duration = buffer.duration;
+      // define the numbr of pixels per seconds the timeline should display
+      var pixelsPerSecond = width / duration;
+      // create a timeline
+      var timeline = new wavesUI.core.Timeline(pixelsPerSecond, width);
+      // create a new track into the `track-1` element and give it a id ('main')
+      timeline.createTrack($track, height, 'main');
 
-    // create the layer
-    var waveformLayer = new wavesUI.helpers.WaveformLayer(buffer, {
-      height: height
+      // create the layer
+      var waveformLayer = new wavesUI.helpers.WaveformLayer(buffer, {
+        height: height
+      });
+
+      // insert the layer inside the 'main' track
+      timeline.addLayer(waveformLayer, 'main');
     });
-
-    // insert the layer inside the 'main' track
-    timeline.addLayer(waveformLayer, 'main');
-  });
+  }
 }
 
 
@@ -57,3 +59,6 @@ function component() {
 }
 
 document.body.appendChild(component());
+const track = new Track();
+
+export default Track;
